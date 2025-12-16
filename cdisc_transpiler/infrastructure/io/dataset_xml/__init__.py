91cdb87a7abd3289132d5e63652d570ce4289276
Ruben Talstra <14235001+rubentalstra@users.noreply.github.com>
2025-12-16 14:22:22 +0100

    refactor: Remove Dataset-XML writer and related infrastructure

--- cdisc_transpiler/infrastructure/io/dataset_xml/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Dataset-XML 1.0 infrastructure implementation.
-
-This is a low-level implementation used by the Dataset-XML writer adapter.
-Prefer importing from the defining modules:
-- cdisc_transpiler.infrastructure.io.dataset_xml.builder
-- cdisc_transpiler.infrastructure.io.dataset_xml.writer
-"""
