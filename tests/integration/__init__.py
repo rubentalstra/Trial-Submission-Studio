55b2a9b489369d91f94badef2734922fbd72b98c
Ruben Talstra <14235001+rubentalstra@users.noreply.github.com>
2025-12-28 19:46:29 +0100

    removed python

--- tests/integration/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Integration tests for CDISC Transpiler application.
-
-This package contains end-to-end integration tests that verify the complete
-workflows using real file system operations and actual data processing.
-"""
